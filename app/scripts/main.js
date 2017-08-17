1a28ac15280f6f6e64d2b72770cad1963232bae2
Hajime Fukuda <hajime.fukuda@ipmu.jp>
2017-08-17 14:55:42 +0900

    forgot to add

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,22 @@
-function main() {
-  let dds = Array.prototype.map.call(document.getElementsByTagName('dd'), e => e);
-  let nucls = dds.filter(dd => {
+function is_nucl(dd) {
     let subs = dd.getElementsByClassName('list-subjects');
     return subs.length > 0 && subs[0].innerText.indexOf('nucl-th') >= 0;
-  });
+}
 
+function mark_radio(nucls) {
   for (let nucl of nucls) {
     nucl.classList.add('ractive');
     nucl.previousElementSibling.classList.add('ractive');
   }
+}
+
+let map = (a, b) => Array.prototype.map.call(a, b);
+let id = a => a;
+let as_map = a => map(a, id)
+
+function main() {
+  let nucls = as_map(document.getElementsByTagName('dd')).filter(is_nucl);
+  mark_radio(nucls);
 }
 
 main();
